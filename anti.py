a6f52066cdc7e3760824415c7261d0223308b092
oLeviChiang <levi.chiang@access-company.com>
2020-04-22 20:27:46 +0800

    Make user all elments < 1

--- anti.py
+++ anti.py
@@ -21,22 +21,24 @@ class Anti():
         return array2D
     
     def __generate_point(self, normal_distribution_value):
-            array = np.empty(self.dimension, dtype=float)
-            max_value = normal_distribution_value
-            sum = 0
-            count = 0
+        array = np.empty(self.dimension, dtype=float)
+        max_value = normal_distribution_value
+        sum = 0
+        count = 0
+        while True:
             while count < self.dimension - 1:
                 random_value = np.random.random_sample() * max_value
                 if random_value < 1:
                     array[count] = (random_value)
-                    max_value -= random_value
+                    max_value = max_value - random_value
                     sum += random_value
                     count += 1
                 else:
                     continue
             array[self.dimension - 1] = normal_distribution_value - sum
-            np.random.shuffle(array)
-            return array
+            if np.all(array < 1):
+                break
+        return array
             
 def main():
     number = int(input("number of points:"))

35a57f863b87c0562186ff313ae49fc05d3bc130
oLeviChiang <levi.chiang@access-company.com>
2020-05-10 15:38:06 +0800

    Add 3d plot

--- plot.py
+++ plot.py
@@ -5,3 +5,20 @@ def plot2D(data):
     x, y = np.array(data).T
     plt.scatter(x,y)
     plt.show()
+
+def plot3D(data):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x = []
+    y = []
+    z = []
+    for point in data:
+        x.append(point[0])
+        y.append(point[1])
+        z.append(point[2])
+    ax.scatter(x, y, z)
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    ax.plot3D([0, 1], [0, 1], [0, 1], 'gray')
+    plt.show()

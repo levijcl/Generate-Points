e9dd7ebde2d8823d14fe5a990661f85ff2dd5d2a
oLeviChiang <levi.chiang@access-company.com>
2020-10-23 16:24:01 +0800

    Update indep.py

--- indep.py
+++ indep.py
@@ -1,23 +1,29 @@
 import numpy as np
 import math
 import random
-from plot import plot2D, plot3D
+from tqdm import tqdm
+from util import PointGenerate
 
-class Indep():
+class Indep(PointGenerate):
     def __init__(self, number, dimension):
         self.number = number
         self.dimension = dimension
-    
+        super().__init__(number, dimension, 1)
+        self.bar = tqdm(total=self.number, desc='Indep-' + str(self.dimension))
+
     def generate(self):
-        array = np.random.rand(self.number, self.dimension)
-        np.savetxt('indep' + str(self.dimension) + 'D-' + str(self.number) + '.txt', array, fmt='%f')
-        return array
+        with open('indep' + str(self.dimension) + 'D-' + str(self.number) + '.txt', 'a') as file:
+            for _ in range(0, self.number):
+                point = np.random.rand(self.dimension)
+                shuffled_point = self.shuffle(point)
+                file.write(' '.join(str("{:0.6f}".format(number)) for number in shuffled_point) + '\n')
+                self.bar.update(1)
 
 def main():
     number = int(input("number of points:"))
     dimension = int(input("dimension:"))
     indep = Indep(number, dimension)
-    data = indep.generate()
+    indep.generate()
 if __name__ == "__main__":
     main()
 
